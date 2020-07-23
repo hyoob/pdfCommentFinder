8a3bc8f34d7c0c33197616df0e4148dd739e6e50
Christian Colleter <chris.colleter@gmail.com>
2020-07-23 22:30:11 +0100

    Add logic to color stringToLookFor in results.

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ from glob import glob
 print("")	
 stringToLookFor = sys.argv[1]
 pdf_files = glob("**/*.pdf", recursive=True)
-# print(pdf_files) 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 for pdf in pdf_files:
 	input1 = PyPDF2.PdfFileReader(open(pdf, "rb"))
@@ -22,10 +30,15 @@ for pdf in pdf_files:
 				subtype = annot.getObject()['/Subtype']
 				if subtype == "/Text":
 					CommentContents = annot.getObject()['/Contents']
-					if CommentContents.find(stringToLookFor) != -1:
+					matchStartIndex = CommentContents.find(stringToLookFor)
+					resultPadding = 15
+					if matchStartIndex != -1:
 						matchNumber = matchNumber + 1
-						startIndex = CommentContents.find(stringToLookFor)
-						matchResults.append(CommentContents[startIndex:startIndex+15])
+						if matchStartIndex - resultPadding < 0: 
+							returnStartIndex = matchStartIndex - resultPadding + (-(matchStartIndex - resultPadding))
+						else:
+							returnStartIndex = matchStartIndex - resultPadding
+						matchResults.append(CommentContents[returnStartIndex:matchStartIndex+resultPadding+len(stringToLookFor)])
 		except :
 		# there are no annotations on this page
 			pass
@@ -33,7 +46,8 @@ for pdf in pdf_files:
 	if matchNumber > 0:
 		print("*** " + pdf + " ***")
 		for result in matchResults:
+			finalIndex = result.find(stringToLookFor)
 			print(" " + "-" * (len(result)+6)) 
-			print("|   " + result + "   |")
+			print("|   " + result[0:finalIndex] + bcolors.FAIL + result[finalIndex:finalIndex+len(stringToLookFor)] + bcolors.ENDC + result[finalIndex+len(stringToLookFor):len(result)] + "   |")
 			print(" " + "-" * (len(result)+6)) 
 		print("")
