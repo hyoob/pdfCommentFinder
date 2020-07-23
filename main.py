b5ba56665fca5a4812e8f571dea293ad38a7fd69
Christian Colleter <chris.colleter@gmail.com>
2020-07-23 21:03:25 +0100

    Change main.py logic to find matches and format results.

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 import sys 
 import PyPDF2
+from glob import glob
 
-try :
-	src = sys.argv[1]
-except :
-	src = r'~/Applications/yoFinder/test.pdf'
+print("")	
+stringToLookFor = sys.argv[1]
+pdf_files = glob("**/*.pdf", recursive=True)
+# print(pdf_files) 
 
-input1 = PyPDF2.PdfFileReader(open(src, "rb"))
-nPages = input1.getNumPages()
+for pdf in pdf_files:
+	input1 = PyPDF2.PdfFileReader(open(pdf, "rb"))
+	nPages = input1.getNumPages()
+	matchNumber = 0
+	matchResults = []
+	for i in range(nPages) :
+	# get the data from this PDF page (first line of text, plus annotations)
+		page = input1.getPage(i)
+		text = page.extractText()
+		try :
+			for annot in page['/Annots'] :
+			# Other subtypes, such as /Link, cause errors
+				subtype = annot.getObject()['/Subtype']
+				if subtype == "/Text":
+					CommentContents = annot.getObject()['/Contents']
+					if CommentContents.find(stringToLookFor) != -1:
+						matchNumber = matchNumber + 1
+						startIndex = CommentContents.find(stringToLookFor)
+						matchResults.append(CommentContents[startIndex:startIndex+15])
+		except :
+		# there are no annotations on this page
+			pass
 
-for i in range(nPages) :
-# get the data from this PDF page (first line of text, plus annotations)
-	page = input1.getPage(i)
-	text = page.extractText()
-
-#	print(text.splitlines()[0])
-#	print('')	
-
-	try :
-		for annot in page['/Annots'] :
-		# Other subtypes, such as /Link, cause errors
-			subtype = annot.getObject()['/Subtype']
-			if subtype == "/Text":
-				print(annot.getObject()['/Contents'])
-	#	print('')
-	except :
-	# there are no annotations on this page
-		pass
-
-# print('')
+	if matchNumber > 0:
+		print("*** " + pdf + " ***")
+		for result in matchResults:
+			print(" " + "-" * (len(result)+6)) 
+			print("|   " + result + "   |")
+			print(" " + "-" * (len(result)+6)) 
+		print("")
